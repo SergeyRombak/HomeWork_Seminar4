b216156dc68d7330191186f94f84b42f147331d1
Sergey Rombak <rombak.a87@mail.ru>
2023-12-03 14:25:02 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача3
+/* Напишите программу, которая перевернёт одномерный массив 
+(первый элемент станет последним, второй – предпоследним и т.д.) */
+
+Console.Clear();
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 101);
+    }
+    return array;
+}
+
+void PrintArray(int[] inputArray)
+{
+    Console.WriteLine();
+    Console.Write("Массив с элементами: ");
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        Console.Write(inputArray[i] + " ");
+    }
+}
+
+void ReverseArray(int[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[length - 1 - i];
+        arr[length - 1 - i] = temp;
+    }
+}
+int SizaArr = 15;
+int[] Arr = FillArray(SizaArr);
+PrintArray(Arr);
+ReverseArray(Arr);
+PrintArray(Arr);
